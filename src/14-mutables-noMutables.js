791f4cb5a6ef3eaed54ea8e16a7730625dac823e
Jorge Arias Argüelles <jorgeariasarguelles@gmail.com>
2021-12-06 23:34:10 +0100

    add/ mutables & no mutables methods

---
+++ src/14-mutables-noMutables.js
@@ -0,0 +1,44 @@
+const products = [
+  { title: 'Pizza', price: 121, id: '🍕' },
+  { title: 'Burger', price: 121, id: '🍔' },
+  { title: 'Hot cakes', price: 121, id: '🥞' },
+];
+
+let misProductos = [];
+
+console.log("Estado ========= 0");
+console.log("Productos:", products);
+console.log("Mis productos:", misProductos)
+
+// 1. Buscar un producto en especifico
+const producto = products.find( p => p.id === '🍔');
+if(producto){
+  misProductos.push(producto);
+}
+console.log("Estado ========= 1");
+console.log("Productos:", products);
+console.log("Mis productos:", misProductos)
+// { title: 'Burger', price: 121, id: '🍔' }
+
+// 2. Agregar un producto a MiProductos y elininarlo de la lista de productos
+const productoIndex = products.findIndex( p => p.id === '🍔');
+if(productoIndex !== -1){
+  [...misProductos].push(products[productoIndex]);
+  products.splice(productoIndex,1);
+}
+
+console.log("Estado ========= 2");
+console.log("Productos:", products);
+console.log("Mis productos:", misProductos)
+
+// 3. Eliminación de un array sin mutar el Original
+
+const prodIndex = products.findIndex( p => p.id === '🍔');
+if(prodIndex !== -1){
+  misProductos.push(products[prodIndex]);
+  products.splice(prodIndex,1);
+}
+
+console.log("Estado ========= 3");
+console.log("Productos:", products);
+console.log("Mis productos:", misProductos)
